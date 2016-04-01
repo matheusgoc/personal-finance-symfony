0a5be91852dbe9e6cede16fd1f77a2bcbba51113
matheusgoc <matheus.goc@gmail.com>
2016-04-01 02:43:55 -0300

    Start Personal Finance System

---
+++ README.md
@@ -0,0 +1,4 @@
+personal-finance
+================
+
+A Symfony project created on January 25, 2016, 10:26 pm.
---
+++ src/AppBundle/Controller/LuckyController.php
@@ -0,0 +1,46 @@
+<?php
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class LuckyController
+{
+    /**
+     * @Route("/lucky/number/{count}")
+     */
+    public function numberAction($count)
+    {
+        $numbers = array();
+        for ($i = 0; $i < $count; $i++) {
+            $numbers[] = rand(0, 100);
+        }
+        $numbersList = implode(', ', $numbers);
+
+        return new Response(
+            '<html><body>Lucky numbers: '.$numbersList.'</body></html>'
+        );
+    }
+    
+    /**
+     * @Route("/api/lucky/number")
+     */
+    public function apiNumberAction()
+    {
+        $data = array(
+            'lucky_number' => rand(0, 100),
+        );
+
+        // calls json_encode and sets the Content-Type header
+        return new JsonResponse($data);
+        
+//        
+//        return new Response(
+//            json_encode($data),
+//            200,
+//            array('Content-Type' => 'application/json')
+//        );
+//        
+    }
+}
